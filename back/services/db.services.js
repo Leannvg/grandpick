287a30496d8ab35aedf03eb7865869532276c73d
Leandro Vedia <leandro.vedia@davinci.edu.ar>
2026-02-04 23:53:02 -0300

    Fix server 2 PORT for Railway

--- back/services/db.services.js
+++ back/services/db.services.js
@@ -1,14 +1,19 @@
 import { MongoClient } from "mongodb";
 
+const uri = process.env.MONGO_URI;
 
-const client = new MongoClient("mongodb://127.0.0.1:27017");
+if (!uri) {
+  throw new Error("❌ MONGO_URI no está definida");
+}
+
+const client = new MongoClient(uri);
 
 let db;
 
 export async function connectDB() {
   if (!db) {
     await client.connect();
-    db = client.db("F1PredictZone");
+    db = client.db(); // usa la DB definida en la URI
     console.log("✅ Mongo conectado");
   }
   return db;
