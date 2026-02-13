749e0f53e2ef7f805d1d05c807d44eaa44704157
Juli <julieta.barias@davinci.edu.ar>
2026-02-12 23:06:11 -0300

    drivers2

---
+++ front/src/components/drivers/DriverCardDesktop.jsx
@@ -0,0 +1,22 @@
+<div class="drivers-grid">
+    <article class="driver-card-horizontal">
+        <div class="driver-info-horizontal">
+            <span class="driver-number-horizontal">43</span>
+            <h3 class="driver-name-horizontal">
+                <span class="driver-firstname-horizontal">FRANCO</span>
+                <span class="driver-lastname-horizontal">COLAPINTO</span>
+            </h3>
+            <div class="driver-tags-horizontal">
+                <span class="tag-horizontal">ARG</span>
+                <span class="tag-horizontal team-tag-horizontal">
+                    <img src="img/Alpine-Iso.png" class="team-logo-horizontal"/>
+                </span>
+                <span class="tag-horizontal">COL</span>
+            </div>
+        </div>
+        <div class="driver-photo-wrapper-horizontal">
+            <img src="img/colapinto.png" alt="Franco Colapinto" class="driver-photo-horizontal"/>
+        </div>
+    </article>
+ </div>
+  
--- front/src/pages/Drivers.jsx
+++ front/src/pages/Drivers.jsx
@@ -12,12 +12,20 @@ function Drivers() {
 
   return (
     <div>
-      <h2>Pilotos</h2>
-      <ul>
-        {drivers.map(({ _id, full_name }) => (
-          <li key={_id}>{full_name}</li>
-        ))}
-      </ul>
+      <section class="drivers-section text-center page-section container">
+       <header class="page-header">
+            <p class="section-label">Temporada actual</p>
+            <h1 class="section-title">PILOTOS</h1>
+            <p class="section-subtitle">Los verdaderos protagonistas de la pista</p>
+        </header>
+
+        <ul>
+          {drivers.map(({ _id, full_name }) => (
+            <li key={_id}>{full_name}</li>
+          ))}
+        </ul>
+
+      </section>
     </div>
   );
 }
