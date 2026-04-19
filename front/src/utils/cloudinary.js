ed8042d552555436f832674e07dfe7470cbc3542
Leandro Vedia <leandro.vedia@davinci.edu.ar>
2026-04-19 20:33:07 -0300

    cambios cloudinary 11

--- front/src/utils/cloudinary.js
+++ front/src/utils/cloudinary.js
@@ -1,6 +1,6 @@
 export const CLOUDINARY_DEFAULTS = {
     PROFILE: "profile_default.png",
-    EMPTY: "imagen_vacia.png",
+    EMPTY: "imagen_no_encontrada.png",
     LOGO: "logo_grandpick.svg",
     VERSION: "v2" // Incrementa esto (v3, v4...) para limpiar el caché si cambias una imagen conservando el mismo nombre
 };
@@ -25,13 +25,13 @@ export function getImageUrl(path, width, type = "empty") {
     if (!cloudName) {
         console.warn("Falta VITE_CLOUDINARY_CLOUD_NAME en el archivo .env");
     }
-    
+
     // Elegir el "Default" de Cloudinary
     // d_NombreDelArchivo: le dice a Cloudinary que si no existe la ruta, devuelva este archivo
     const defaultImg = type === "profile" ? CLOUDINARY_DEFAULTS.PROFILE : CLOUDINARY_DEFAULTS.EMPTY;
     const defaultTransform = `d_${defaultImg}`;
 
     const widthTransform = width ? `,w_${width}` : "";
-    
+
     return `https://res.cloudinary.com/${cloudName}/image/upload/${defaultTransform},f_auto,q_auto${widthTransform}/${CLOUDINARY_DEFAULTS.VERSION}/${cleanPath}`;
 }
