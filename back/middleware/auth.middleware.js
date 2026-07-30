71877786289525a9dc4be216daf3b62d3072e758
Leandro Vedia <leandro.vedia@davinci.edu.ar>
2026-07-30 00:15:08 -0300

    arreglos generales 2

--- back/middleware/auth.middleware.js
+++ back/middleware/auth.middleware.js
@@ -36,60 +36,36 @@ function autenticado(req, res, next) {
 
 
 function admin(req, res, next) {
-    console.log(req)
-    if (!req.usuario) {
-        return res.status(401).json({ message: 'No autenticado' })
-    }
+  console.log(req)
+  if (!req.usuario) {
+    return res.status(401).json({ message: 'No autenticado' })
+  }
 
-    if (req.usuario.rol !== 'admin') {
-        return res.status(403).json({
-            message: 'No tienes permisos de administrador'
-        })
-    }
+  if (req.usuario.rol !== 'admin') {
+    return res.status(403).json({
+      message: 'No tienes permisos de administrador'
+    })
+  }
 
-    next()
+  next()
 }
 
 
-/* function admin(req, res, next) {
-    const token = req.headers['auth-token']
-
-    if(!token) {
-        return res.status(401).json({ message: 'No se envió un token' });
-    }
-
-    try {
-        const payload = jwt.verify(token, 'grandpick123');
-        usersServices.getUserById(payload.id)
-        .then(usuario =>{
-            if (usuario.rol !== 'admin'){
-                return res.status(401).json({ message: 'No tienes las credenciales necesarias para realizar esta acción' })
-            }
-            console.log("SOS ADMIN")
-        })
-        next();
-    } catch (err){
-        console.log("NO SOS ADMIN")
-        return res.status(401).json({ message: 'Token inválido' });
-    }
-} */
-
-
 function selfOrAdmin(req, res, next) {
-    const userId = req.params.id
+  const userId = req.params.id
 
-    if (req.usuario.rol === 'admin' || req.usuario._id == userId) {
-        return next()
-    }
+  if (req.usuario.rol === 'admin' || req.usuario._id == userId) {
+    return next()
+  }
 
-    return res.status(403).json({
-        message: 'No tienes permiso para acceder a este recurso'
-    })
+  return res.status(403).json({
+    message: 'No tienes permiso para acceder a este recurso'
+  })
 }
 
 
 export {
-    autenticado,
-    admin,
-    selfOrAdmin
+  autenticado,
+  admin,
+  selfOrAdmin
 }
